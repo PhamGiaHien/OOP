193d01b7c55a0f445e7857f15015ea2c4f779874
PhamGiaHien <phamgiahien1205@gmail.com>
2025-09-20 17:31:36 +0700

    check commit

--- oop-main/HW1/HelloWorld.java
+++ oop-main/HW1/HelloWorld.java
@@ -1,9 +1,39 @@
-public class HelloWorld {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
+public class Solution {
 
-        // Prints "Hello, World" in the terminal window.
-        System.out.println("Hello, World");
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+
+        int size = s.length();
+        smallest = s.substring(0, k);
+        largest = s.substring(0, k);
+        for (int i = 1; i < size; i++){
+
+            String sub = s.substring(i, k + i);
+            if (sub.compareTo(smallest) < 0){
+                smallest = sub;
+            }
+            if (sub.compareTo(largest) > 0){
+                largest = sub;
+            }
+
+        }
+        return smallest + "\n" + largest;
     }
 
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+
+        System.out.println(getSmallestAndLargest(s, k));
+    }
 }
